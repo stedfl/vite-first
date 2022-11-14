eeeb97108c2f5d42437eca47a2ab3f35ff871f4c
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-11-14 15:05:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  name: 'App',
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Hello!</h1>
+    <AppTitle/>
+  </div>
+  
+</template>
+
+
+
+<style>
+.container {
+  width: 70%;
+  margin: 0 auto;
+}
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'AppTitle',
+  data() {
+    return {
+      title: 'La mia prima App con Vite'
+    }
+  }
+  
+
+}
+</script>
+
+<template>
+
+  <h2>{{title}}</h2>
+  
+</template>
+
+
+
+<style>
+
+</style>
